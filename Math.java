dd01fe2fffc0d8b0bebf72dd68041e86a713da9b
JiankaiZhong <93751654+1062644@users.noreply.github.com>
2023-02-06 11:27:32 -0800

    Create Math.java

---
+++ Math.java
@@ -0,0 +1,22 @@
+import java.util.Scanner; 
+public class time {
+public static void main(String[]args) {
+        //initializing four integers
+        int a=4, b=6, c=-5, d=8;
+        System.out.println(Math.min(a, b));   
+        System.out.println(Math.pow(a, b));   
+        System.out.println(Math.sqrt(a));   
+        System.out.println(Math.sqrt(d));  
+        System.out.println(Math.abs(c));   
+        System.out.println("-------------------------------");   
+        int a2=60;
+        System.out.println(Math.cos(a2));   
+        System.out.println(Math.tan(a2));   
+        System.out.println("------------------------------");   
+        int a3=100;
+        System.out.println(Math.log10(a3));  
+        System.out.println("------------------------------");  
+        Scanner input = new Scanner(System.in); 
+        int a4=input.nextInt(); 
+        System.out.println(Math.sqrt(a4));  
+}}
